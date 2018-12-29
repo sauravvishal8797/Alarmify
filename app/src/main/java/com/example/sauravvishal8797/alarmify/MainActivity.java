eb7c3f3af39d716ffe12fb824b680ca3659f1fbd
sauravvishal8797 <sauravvishal8797@gmail.com>
2018-12-30 02:09:18 +0530

    Added basic layout for MainActivity

--- app/src/main/java/com/example/sauravvishal8797/alarmify/MainActivity.java
+++ app/src/main/java/com/example/sauravvishal8797/alarmify/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sauravvishal8797.alarmify;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        statusBarTransparent();
+    }
+
+    //method to set the status bar transparent
+    private void statusBarTransparent(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 }
