74dd9e6004abdb37b22e74da965ff4920e4f537a
sauravvishal8797 <sauravvishal8797@gmail.com>
2018-12-30 18:55:10 +0530

    Added alarmDetail layout

---
+++ app/src/main/java/com/example/sauravvishal8797/alarmify/AlarmDetail.java
@@ -0,0 +1,82 @@
+package com.example.sauravvishal8797.alarmify;
+
+import android.content.res.Resources;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
+
+import java.lang.reflect.Field;
+
+public class AlarmDetail extends AppCompatActivity {
+
+    private TimePicker time_picker;
+    private Resources system;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alarm_detail);
+        time_picker = (TimePicker) findViewById(R.id.timepicker22);
+        statusBarTransparent();
+        setTimePickerTextColor();
+    }
+
+    //method to set the status bar transparent
+    private void statusBarTransparent(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+    }
+
+    private void setTimePickerTextColor(){
+        system = Resources.getSystem();
+        int hour_numberpicker_id = system.getIdentifier("hour", "id", "android");
+        int minute_numberpicker_id = system.getIdentifier("minute", "id", "android");
+        int ampm_numberpicker_id = system.getIdentifier("amPm", "id", "android");
+
+        NumberPicker hour_numberpicker = (NumberPicker) time_picker.findViewById(hour_numberpicker_id);
+        NumberPicker minute_numberpicker = (NumberPicker) time_picker.findViewById(minute_numberpicker_id);
+        NumberPicker ampm_numberpicker = (NumberPicker) time_picker.findViewById(ampm_numberpicker_id);
+
+        set_numberpicker_text_colour(hour_numberpicker);
+        set_numberpicker_text_colour(minute_numberpicker);
+        set_numberpicker_text_colour(ampm_numberpicker);
+
+    }
+
+    private void set_numberpicker_text_colour(NumberPicker number_picker){
+        final int count = number_picker.getChildCount();
+        final int color = getResources().getColor(R.color.grey_shade);
+
+        for(int i = 0; i < count; i++){
+            View child = number_picker.getChildAt(i);
+
+            try{
+                Field wheelpaint_field = number_picker.getClass().getDeclaredField("mSelectorWheelPaint");
+                wheelpaint_field.setAccessible(true);
+
+                ((Paint)wheelpaint_field.get(number_picker)).setColor(color);
+                ((EditText)child).setTextColor(color);
+                number_picker.invalidate();
+            }
+            catch(NoSuchFieldException e){
+                Log.w("setNumberPickerText", e);
+            }
+            catch(IllegalAccessException e){
+                Log.w("setNumberPickerTex", e);
+            }
+            catch(IllegalArgumentException e){
+                Log.w("setNumberPickerText", e);
+            }
+        }
+    }
+}
